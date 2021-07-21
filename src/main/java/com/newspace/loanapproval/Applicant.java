689c95f46cff6a8383b6f4df8a6fc2fddaef3b09
wbadmin <>
2021-07-21 15:09:50 -0500

    {/src/main/java/com/newspace/loanapproval/Applicant.java}

--- src/main/java/com/newspace/loanapproval/Applicant.java
+++ src/main/java/com/newspace/loanapproval/Applicant.java
@@ -6,12 +6,58 @@ package com.newspace.loanapproval;
 
 public class Applicant implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Applicant() {
-    }
+	@org.kie.api.definition.type.Label(value = "Age")
+	private java.lang.Integer age;
+	@org.kie.api.definition.type.Label(value = "Approved")
+	private java.lang.Boolean approved;
+	@org.kie.api.definition.type.Label(value = "Money")
+	private java.lang.Double money;
+	@org.kie.api.definition.type.Label(value = "Name")
+	private java.lang.String name;
 
+	public Applicant() {
+	}
 
+	public java.lang.Integer getAge() {
+		return this.age;
+	}
 
+	public void setAge(java.lang.Integer age) {
+		this.age = age;
+	}
+
+	public java.lang.Boolean getApproved() {
+		return this.approved;
+	}
+
+	public void setApproved(java.lang.Boolean approved) {
+		this.approved = approved;
+	}
+
+	public java.lang.Double getMoney() {
+		return this.money;
+	}
+
+	public void setMoney(java.lang.Double money) {
+		this.money = money;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public Applicant(java.lang.Integer age, java.lang.Boolean approved,
+			java.lang.Double money, java.lang.String name) {
+		this.age = age;
+		this.approved = approved;
+		this.money = money;
+		this.name = name;
+	}
 
 }
